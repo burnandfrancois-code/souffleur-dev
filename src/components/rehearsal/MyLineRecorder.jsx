56a06944cada716ca3d4c6ed8436d8cfc74969c9
François Burnand (FB) <burnandfrancois@gmail.com>
2026-05-09 23:46:40 +0000

    File changes

--- src/components/rehearsal/MyLineRecorder.jsx
+++ src/components/rehearsal/MyLineRecorder.jsx
@@ -180,7 +180,7 @@ const MyLineRecorder = forwardRef(function MyLineRecorder({ line, onSubmit, onSk
     if (autoPlay && !trainingMode) {
       const timer = setTimeout(() => {
         startRecordingRef.current();
-      }, 100);
+      }, 400);
       return () => clearTimeout(timer);
     }
   }, [autoPlay, trainingMode, line]);
