d50321f2cb6fdd80298711a86f992b921a7320c4
François Burnand (FB) <burnandfrancois@gmail.com>
2026-05-10 22:11:16 +0000

    File changes

--- src/lib/speechServices.js
+++ src/lib/speechServices.js
@@ -56,7 +56,7 @@ function getVoices() {
   });
 }
 
-export async function speakText(text, lang = 'fr-FR', gender = 'male', rate = 1, signal) {
+export async function speakText(text, lang = 'fr-FR', gender = 'male', rate = 1.3, signal) {
   if (signal?.aborted) return;
 
   // Déverrouiller l'audio sur desktop si nécessaire
