05bf181e513a3a23a6ad24c9647834403499c4c6
François Burnand (FB) <burnandfrancois@gmail.com>
2026-05-11 02:08:27 +0000

    File changes

--- src/components/rehearsal/MyLineRecorderAndroid.jsx
+++ src/components/rehearsal/MyLineRecorderAndroid.jsx
@@ -253,15 +253,6 @@ const MyLineRecorderAndroid = forwardRef(function MyLineRecorderAndroid({ line, 
         </div>
       )}
 
-      {/* Debug Logs */}
-      {debugLogs.length > 0 && (
-        <div className="bg-black border border-yellow-500/30 rounded-lg p-2 max-h-24 overflow-y-auto text-xs text-yellow-400 font-mono space-y-0.5">
-          {debugLogs.map((log, i) => (
-            <div key={i}>{log}</div>
-          ))}
-        </div>
-      )}
-
       {/* Transcript */}
       <div className="bg-background border border-border rounded-lg px-3 py-2 min-h-[2rem] flex items-center">
         <p className="text-sm text-foreground">
